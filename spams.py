5de29bef9783033eae744f7cbd2a3e99b5c73fa9
FARUM <116953230+NikolayPy@users.noreply.github.com>
2023-01-29 09:03:29 +0600

    Update spams.py

--- spams.py
+++ spams.py
@@ -4,21 +4,23 @@ from PIL import Image
 import re
 import asyncio
 
-app = Client('session', api_id=9470354, api_hash='09583bf3a3bf5f4824fb68faa8f61dad')
+app = Client('session', api_id=9470354, api_hash='09583bf3a3bf5f4824fb68faa8f61dad') #вместо api_id и api_hash подставляем свои данные с https://my.telegram.org/auth
 
-chat_id = "@AnonRuBot"
+chat_id = "@AnonRuBot" #Сюда айди чата
 
 @app.on_message(chat_id)
 async def spam(client: Client, message: types.Message):
     if "Собеседник найден 😺" in message.text:
-        sleep(2)
-        await app.send_sticker(chat_id,"CAACAgIAAxkBAAEHabtjz9yi9SCOxWiI0WB5-KUxs5ARVQAC5ygAAlqzgUrc1w118xlZ8y0E")
-        sleep(2)
+        sleep(2) #Это задержки между сообщениями 
+        await app.send_sticker(chat_id,"CAACAgIAAxkBAAEHabtjz9yi9SCOxWiI0WB5-KUxs5ARVQAC5ygAAlqzgUrc1w118xlZ8y0E") #В кавычки айди стикера которое берем тут @idstickerbot
+        sleep(2) #Это задержки между сообщениями 
         if "Собеседник найден 😺" in message.text:
             await app.send_message(chat_id,"/stop")
-            sleep(1)
+            sleep(1) #Это задержки между сообщениями 
             await app.send_message(chat_id,"🚀 Поиск любого собеседника")
     if "Собеседник закончил с вами связь 😞" in message.text:
         await app.send_message(chat_id,"🚀 Поиск любого собеседника")
 
 app.run()
+
+#Spamer by anon_chats by FARUM
