c8fb8e199d52197a9cdb529319618a9878a5c0cb
FARUM <116953230+NikolayPy@users.noreply.github.com>
2023-01-29 08:55:06 +0600

    Update spams.py

--- spams.py
+++ spams.py
@@ -8,18 +8,17 @@ app = Client('session', api_id=9470354, api_hash='09583bf3a3bf5f4824fb68faa8f61d
 
 chat_id = "@AnonRuBot"
 
-file1 = "text.txt"
-with open (file1, 'r') as file:
-    text = file.read()
-
 @app.on_message(chat_id)
 async def spam(client: Client, message: types.Message):
     if "Собеседник найден 😺" in message.text:
-        #await app.send_message(chat_id,text)
+        sleep(2)
         await app.send_sticker(chat_id,"CAACAgIAAxkBAAEHabtjz9yi9SCOxWiI0WB5-KUxs5ARVQAC5ygAAlqzgUrc1w118xlZ8y0E")
         sleep(2)
-        if 'Собеседник найден 😺' in message.raw_text:
+        if "Собеседник найден 😺" in message.text:
             await app.send_message(chat_id,"/stop")
             sleep(1)
             await app.send_message(chat_id,"🚀 Поиск любого собеседника")
+    if "Собеседник закончил с вами связь 😞" in message.text:
+        await app.send_message(chat_id,"🚀 Поиск любого собеседника")
+
 app.run()
